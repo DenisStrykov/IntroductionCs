2e7bf7b91906587c91343db1b8bf968df8705baf
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-17 20:59:01 +0300

    add home work 011

---
+++ 011/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+
+// Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+
+
+
+System.Console.Write("Введите значение больше 9: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Вторая цифра числа с конца равна: ");
+System.Console.WriteLine(a/10%10);
