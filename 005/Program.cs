9dea13365423173477c1c989723e3b38f2367a5f
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-17 21:51:41 +0300

    add home work 005

---
+++ 005/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+// С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+
+
+
+System.Console.Write("Введите 1 число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите 2 число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите 3 число: ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double max = a;
+
+if (max<b) {
+    max = b;
+} 
+if (max<c) {
+    max = c;
+} 
+
+System.Console.WriteLine($"Максимальное из трех чисел равно: {max}");
