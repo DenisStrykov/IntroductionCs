e1757b1f8d3762f506fcf153c662f4aacca093cc
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-17 21:23:34 +0300

    add home work 003

---
+++ 003/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+// С клавиатуры вводятся два числа a и b. Найти максимальное из них
+
+
+
+System.Console.Write("Введите 1 чило: ");
+double a = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите 2 чило: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a>b) {
+    System.Console.WriteLine($"Наибольшее из двух чисел: {a}");
+} else {
+    System.Console.WriteLine($"Наибольшее из двух чисел: {b}");
+}
