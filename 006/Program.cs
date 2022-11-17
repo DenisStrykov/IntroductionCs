60d0e415f53d60c28763738587711fa6f8b1552a
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-17 19:48:37 +0300

    add home work 006

---
+++ 006/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+
+// Написать программу вычисления значения функции y = sin(a). (Класс Math).
+
+
+System.Console.Write("Введите значение для вычисления функции y = sin(a): ");
+double a = Convert.ToDouble(Console.ReadLine());
+double y = Math.Sin(a);
+System.Console.WriteLine(y);
