419ceb133fc98b87ad1db593d493406d9d285b6a
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-17 22:47:51 +0300

    add home work 015

---
+++ 015/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+
+// С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO)
+
+
+
+System.Console.Write("Введите целое число > 99 и < 1000: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a>99 & a<1000) {
+    int b = a-(a/10*10);
+    System.Console.WriteLine($"Третья цифра числа {a} = {b}");
+} else {
+    System.Console.WriteLine("NO");
+}
