05347b8d4694be666f0cac1a498aa5cade5f7689
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-17 21:57:59 +0300

    add home work 007

---
+++ 007/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+
+// Выяснить является ли число чётным
+
+
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2 == 0) {
+    System.Console.WriteLine($"Число {a} действительно четное");
+} else {
+    System.Console.WriteLine($"Число {a} не четное");
+}
