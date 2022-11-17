09060401f2ae5b069f370f4adc855d7c6c1eb0e8
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-17 21:11:03 +0300

    add home work 013

---
+++ 013/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+
+// Удалить вторую цифру целого числа введенного с клавиатуры
+
+
+
+System.Console.Write("Введите значение: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = a/100*10+a%10;
+System.Console.WriteLine($"Значение без второй цифры равно: {b}");
