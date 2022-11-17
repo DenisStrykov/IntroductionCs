da9ba352712e4912a9a520e48363c16582ce3357
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-17 22:29:34 +0300

    add home work 012

---
+++ 012/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+
+// С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+
+
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = a/10;
+int secondNumber = a%10;
+
+if (firstNumber>secondNumber) {
+    System.Console.WriteLine($"Наибольшая цифра числа {a}: {firstNumber}");
+} else {
+    System.Console.WriteLine($"Наибольшая цифра числа {a}: {secondNumber}");
+}
