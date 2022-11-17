de21172239e043eb1d8493c9b4962909f88c9aaa
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-17 20:45:06 +0300

    add home work 009

---
+++ 009/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+// Вывести на экран четные числа от 1 до N
+
+
+System.Console.Write("Введите значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i < n; i = i+2) {
+    System.Console.WriteLine(i);
+}
