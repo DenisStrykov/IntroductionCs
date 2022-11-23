1e03a5395e8f6bc705b2c69dbf8657b969ec2609
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-23 22:40:08 +0300

    time pause at 1.15

---
+++ Basic Algorithm/001-Swap/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+
+// Обмен значениями двух переменных
+
+
+
+/* int a = 5;
+int b = 7;
+
+int c;
+                                    Классический способ
+c = a;
+a = b;
+b= c;
+
+System.Console.WriteLine(a);
+System.Console.WriteLine(b); */
+
+int a =  5;
+int b= 7;
+
+a = a+b;
+b = a-b;
+a = a-b;
+
+System.Console.WriteLine(a);
+System.Console.WriteLine(b);
