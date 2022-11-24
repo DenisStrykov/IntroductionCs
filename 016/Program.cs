0abe3009e6cc53030b0cec6ac071bd6e368b9ad1
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-24 21:30:54 +0300

    add example 016

---
+++ 016/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+
+// Дано число, проверить, кратно ли оно 7 и 23
+
+
+
+System.Console.Write("Введите число для проверки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n%7==0)
+{
+    if (n%23==0)
+    {
+        System.Console.WriteLine($"Введенное вами число {n} кратно 7 и кратно 23");
+    }
+} else
+{
+    System.Console.WriteLine($"Введенное вами число {n} не кратно 7 и не кратно 23");
+}
---
+++ Basic Algorithm/003-min_max/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+int min = int.MaxValue; // int.MaxValue - максимальное значение интовой переменной...
+
+for (int i=0; i<n; i++)
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a<min && a%10==4)
+    {
+        min = a;
+    }
+}
+
+System.Console.WriteLine(min);
