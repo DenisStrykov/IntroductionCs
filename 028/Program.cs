d4cb45b68e6391a3d57bbf53408e67f2c8fe4fc0
DenisStrykov <112210751+DenisStrykov@users.noreply.github.com>
2022-11-26 21:22:06 +0300

    Доделай дз 3

---
+++ 028/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+
+// Определить количество цифр в числе. Сделать подпрограмму.
+
+
+
+System.Console.Write("Введите число, чтобы мы могли подсчитать, из скольких цифр оно состоит: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Введенное вами число состоит из {NumericCounter(number)} цифр(ы)");
+
+
+int NumericCounter(int Number)
+{
+    Number = Math.Abs(Number); // Math.Abs() - откидывает - у числа, приводя его в положительное значение (*(-1))
+    int count = 0;
+    if (Number==0)
+    {
+        count = 1;
+    }
+    while (Number>0)
+    {
+        Number = Number/10;
+        count++;
+    }
+    return count;
+}
---
+++ 031/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+// Сгенерировать 10 случайных чиселю. Показать кубы тех чисел,которые заканчивающихся на четную цифру
+
+
+
+Random random = new Random();
+for (int i=0; i<10; i++)
+{
+    int a = random.Next(1, 100);
+    int b = (int)Math.Pow(a, 3);
+    if (Test(b))
+    {
+    System.Console.WriteLine($"{a}, {b}");
+    }
+}
+
+bool Test(int number)
+{
+    /* if (number%2 == 0)
+    {
+        return true;
+    } else
+    {
+        return false;
+    }*/
+    return number%2 == 0;
+}
